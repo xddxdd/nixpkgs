7dc38ab46150930cddd51cff95e5f41508805ce5
Lan Tian <xuyh0120@outlook.com>
2024-12-07 14:33:39 -0800

    mautrix-gmessages: init at 0.5.2

---
+++ pkgs/by-name/ma/mautrix-gmessages/package.nix
@@ -0,0 +1,38 @@
+{
+  fetchFromGitHub,
+  lib,
+  buildGoModule,
+  olm,
+  # This option enables the use of an experimental pure-Go implementation of
+  # the Olm protocol instead of libolm for end-to-end encryption. Using goolm
+  # is not recommended by the mautrix developers, but they are interested in
+  # people trying it out in non-production-critical environments and reporting
+  # any issues they run into.
+  withGoolm ? false,
+}:
+
+buildGoModule rec {
+  pname = "mautrix-gmessages";
+  version = "0.5.2";
+  src = fetchFromGitHub {
+    owner = "mautrix";
+    repo = "gmessages";
+    rev = "v${version}";
+    hash = "sha256-3sx2XxzH/cj2zJIkV9QKKVadxSWwf4YZbdg3p4repqs=";
+  };
+
+  vendorHash = "sha256-8KexkPsRqzlVeYMF0dVgDMS38zQ3i+erQgsdgHDOCF0=";
+
+  buildInputs = lib.optional (!withGoolm) olm;
+  tags = lib.optional withGoolm "goolm";
+
+  doCheck = false;
+
+  meta = with lib; {
+    homepage = "https://github.com/mautrix/gmessages";
+    description = "Matrix-Google Messages puppeting bridge";
+    license = licenses.agpl3Plus;
+    maintainers = with maintainers; [ xddxdd ];
+    mainProgram = "mautrix-gmessages";
+  };
+}
